0be724fc79c58179d08beaed52a9879d056ab97f
h <huhuqaq@gmail.com>
2022-08-09 14:01:15 +0800

    chore: Add modules, env setting

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
-const indexRouter = require("./routes/posts.route");
+const postsRouter = require("./routes/posts.route");
 const usersRouter = require("./routes/users.route");
-
-const { appError } = require("./services/error.service");
 
 const app = express();
 
 require("dotenv").config();
 require("./services/db.service.js");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(postsRouter);
+app.use(usersRouter);
 
+// Path not found
 app.use((req, res) => {
   res.status(404).json({
     status: "error",
