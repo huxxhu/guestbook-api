6a4e830fbad225f23ea8664c76feba39273ab507
h <huhuqaq@gmail.com>
2022-08-10 18:12:34 +0800

    ✨ (post): Add pagination params

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -3,10 +3,38 @@ const resSuccess = require("../services/success.service");
 const { asyncError, appError } = require("../services/error.service");
 
 module.exports = {
-  getPosts: async (req, res, next) => {
-    const post = await Post.find().sort({ createdAt: -1 });
-    resSuccess({ res, post });
-  },
+  // Get posts
+  getPosts: asyncError(async (req, res, next) => {
+    let {
+      query: { page = 1, limit = 10 },
+    } = req;
+
+    if (!(parseInt(page) >= 1) || !(parseInt(limit) >= 1))
+      return appError(400, "`page` & `limit` should be Number.", next);
+
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    const total = await Post.countDocuments();
+    const pageTotal = Math.ceil(total / limit);
+
+    if (page > pageTotal)
+      return appError(400, "`page` can't be larger than total pages.", next);
+
+    const post = await Post.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const pagination = {
+      pageIndex: page,
+      pageSize: limit,
+      pageTotal,
+      total,
+    };
+    resSuccess({ res, data: post, pagination });
+  }),
+  // Add post
   addPost: asyncError(async (req, res, next) => {
     const {
       ip,
@@ -25,6 +53,6 @@ module.exports = {
       ip,
     });
 
-    resSuccess({ res, newPost });
+    resSuccess({ res, data: newPost });
   }),
 };
